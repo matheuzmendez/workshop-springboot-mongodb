e5794cc1b88daccf3247097419d77a4d11405d14
Matheus Mendes <mendesm13.m@gmail.com>
2022-11-18 15:02:47 -0300

    Delecao de usuario por metodo DELETE

--- src/main/java/com/mendes/workshopmongo/services/UserService.java
+++ src/main/java/com/mendes/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
 
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
